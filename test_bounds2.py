89fd7359506b3e23af5b25b1a1ca303f62fc3f1c
dheerajkumargehlot-web <dheeraj.kumargehlot@students.iiserpune.ac.in>
2026-04-03 14:07:00 +0530

    Delete test_bounds2.py

--- test_bounds2.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.DataFrame({'id':['1'],'v':[1]})
-fig = px.choropleth_mapbox(df, locations='id')
-
-# Try setting to None
-fig.update_layout(mapbox=dict(center=None, zoom=None, bounds={"west": 70, "east": 80, "south": 10, "north": 20}))
-print(fig.layout.mapbox)
